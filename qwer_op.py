69a1704d6c16b14952414d85935561ab71837de8
dpdpforlife <33341095+dpdpforlife@users.noreply.github.com>
2019-04-25 21:08:19 -0500

    Added better select tool tweak controls

--- qwer_op.py
+++ qwer_op.py
@@ -1,11 +1,12 @@
 import bpy
+from bpy.props import StringProperty
 
 class Qwer_OT_Operator(bpy.types.Operator):
     bl_idname = "view3d.qwer_controls"
     bl_label = "qwer Controls"
     bl_description = "qwer Controls"
 
-    mode = bpy.props.StringProperty()
+    mode: StringProperty()
     
     def execute(self, context):        
         areas = bpy.context.workspace.screens[0].areas
@@ -47,3 +48,38 @@ class Qwer_OT_Operator(bpy.types.Operator):
                     if space.type == 'VIEW_3D':
                         space.show_gizmo_object_scale^= True 
         return {'FINISHED'}
+
+class TweakBetter(bpy.types.Operator):
+    """Fix the select active tool"""
+    bl_idname = "object.tweak_better"
+    bl_label = "Tweak Better"
+
+    tmode: StringProperty()
+
+    def modal(self, context, event):
+        if event.type == 'MOUSEMOVE':
+            bpy.ops.transform.transform(
+                            'INVOKE_DEFAULT',
+                            mode=self.tmode)
+            return {'FINISHED'}
+
+        return {'RUNNING_MODAL'}
+
+    def invoke(self, context, event):
+        if context.object:
+            
+            if context.space_data.show_gizmo_object_translate==True:
+                self.tmode = 'TRANSLATION'
+            elif context.space_data.show_gizmo_object_rotate==True:
+                self.tmode = 'ROTATION'
+            elif context.space_data.show_gizmo_object_scale==True:
+                self.tmode = 'RESIZE'
+            elif context.space_data.show_gizmo_object_rotate==True:
+                self.tmode = 'ROTATION'
+            else: self.tmode = 'TRANSLATION'
+                
+            context.window_manager.modal_handler_add(self)
+            return {'RUNNING_MODAL'}
+        else:
+            self.report({'WARNING'}, "No active object, could not finish")
+            return {'CANCELLED'}
