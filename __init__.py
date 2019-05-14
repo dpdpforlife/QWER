eb56781efb28d7650ff897ce25c6ee79df00e340
dpdpforlife <33341095+dpdpforlife@users.noreply.github.com>
2019-05-14 11:37:18 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,32 @@
+bl_info = {
+    "name" : "qwer_addon",
+    "author" : "Dan Pool (dpdp)",
+    "description" : "QWER Addon",
+    "blender" : (2, 80, 0),
+    "location" : "View3D",
+    "warning" : "",
+    "category" : "Generic"
+}
+
+import bpy
+
+from . qwer_op import Qwer_OT_Operator, Cycle_OT_Operator, TweakBetter
+from . qwer_panel import Qwer_PT_Panel,draw_topbar
+
+def register():
+    bpy.utils.register_class(Qwer_OT_Operator)
+    bpy.utils.register_class(Cycle_OT_Operator)
+    bpy.utils.register_class(TweakBetter)
+    bpy.utils.register_class(Qwer_PT_Panel)
+    bpy.types.VIEW3D_HT_tool_header.append(draw_topbar)
+
+def unregister():
+    bpy.utils.unregister_class(Qwer_OT_Operator)
+    bpy.utils.unregister_class(Cycle_OT_Operator)
+    bpy.utils.unregister_class(TweakBetter)
+    bpy.utils.unregister_class(Qwer_PT_Panel)
+    bpy.types.VIEW3D_HT_tool_header.remove(draw_topbar)
+
+if __name__ == "__main__":
+    register()
+    
---
+++ qwer_panel.py
@@ -0,0 +1,60 @@
+import bpy
+
+class Qwer_PT_Panel(bpy.types.Panel):
+    bl_idname = "QWER_PT_Panel"
+    bl_label = "QWER Panel"
+    bl_category = "QWER Addon"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    
+    def draw(self, context):
+        dep_move = False
+        dep_rot = False
+        dep_scale = False
+
+        area = next(area for area in bpy.context.screen.areas if area.type == 'VIEW_3D')
+        space = next(space for space in area.spaces if space.type == 'VIEW_3D')
+    
+        if space.show_gizmo_object_translate:
+            dep_move = True
+        if space.show_gizmo_object_rotate:
+            dep_rot = True
+        if space.show_gizmo_object_scale:
+            dep_scale = True
+        
+        layout = self.layout
+        
+        row = layout.row(align=True)
+        row.operator('view3d.qwer_controls', text="Move", depress=dep_move).move = True
+        row.operator('view3d.qwer_controls', text="Rotate", depress=dep_rot).rotate = True
+        row.operator('view3d.qwer_controls', text="Scale", depress=dep_scale).scale = True
+        row = layout.row(align=True)
+        row.operator('view3d.qwer_controls', text="AMove").mode = "AddMove"
+        row.operator('view3d.qwer_controls', text="ARotate").mode = "AddRotate"
+        row.operator('view3d.qwer_controls', text="AScale").mode = "AddScale"
+        row = layout.row(align=True)
+        row.operator('view3d.qwer_cycle', text="Cycle Gizmo Visibility")
+        
+def draw_topbar(self, context):
+    dep_move = False
+    dep_rot = False
+    dep_scale = False
+
+    area = next(area for area in bpy.context.screen.areas if area.type == 'VIEW_3D')
+    space = next(space for space in area.spaces if space.type == 'VIEW_3D')
+    
+    if space.show_gizmo_object_translate:
+        dep_move = True
+    if space.show_gizmo_object_rotate:
+        dep_rot = True
+    if space.show_gizmo_object_scale:
+        dep_scale = True
+
+    if context.region.alignment != 'RIGHT':
+        layout = self.layout
+        row = layout.row(align=True)
+        row.operator('view3d.qwer_controls', text="M", depress=dep_move).move = True
+        row.operator('view3d.qwer_controls', text="R", depress=dep_rot).rotate = True
+        row.operator('view3d.qwer_controls', text="S", depress=dep_scale).scale = True
+
+    
