81d89f68211be6a1527f7fbd2b2cabca7ed1d30d
dpdpforlife <33341095+dpdpforlife@users.noreply.github.com>
2019-05-21 17:23:43 -0500

    Fixing Shift Keypresses

--- qwer_op.py
+++ qwer_op.py
@@ -14,7 +14,44 @@ class Qwer_OT_Operator(bpy.types.Operator):
     def invoke(self, context, event):
         
         areas = bpy.context.workspace.screens[0].areas
- 
+        areas = bpy.context.workspace.screens[0].areas
+
+        if self.mode == "Move":
+            for area in areas:
+                for space in area.spaces:
+                    if space.type == 'VIEW_3D':
+                        space.show_gizmo_object_translate^= True
+                        space.show_gizmo_object_rotate = False
+                        space.show_gizmo_object_scale = False
+        if self.mode == "Rotate":
+            for area in areas:
+                for space in area.spaces:
+                    if space.type == 'VIEW_3D':
+                        space.show_gizmo_object_translate = False
+                        space.show_gizmo_object_rotate^= True
+                        space.show_gizmo_object_scale = False
+        if self.mode == "Scale":
+            for area in areas:
+                for space in area.spaces:
+                    if space.type == 'VIEW_3D':
+                        space.show_gizmo_object_translate = False
+                        space.show_gizmo_object_rotate = False
+                        space.show_gizmo_object_scale^= True
+        if self.mode == "AddMove":
+            for area in areas:
+                for space in area.spaces:
+                    if space.type == 'VIEW_3D':
+                        space.show_gizmo_object_translate^= True
+        if self.mode == "AddRotate":
+            for area in areas:
+                for space in area.spaces:
+                    if space.type == 'VIEW_3D':
+                        space.show_gizmo_object_rotate^= True
+        if self.mode == "AddScale":
+            for area in areas:
+                for space in area.spaces:
+                    if space.type == 'VIEW_3D':
+                        space.show_gizmo_object_scale^= True
         if self.move:
             if event.shift:
                 for area in areas:
@@ -57,43 +94,6 @@ class Qwer_OT_Operator(bpy.types.Operator):
                             space.show_gizmo_object_rotate = False
                             space.show_gizmo_object_scale^= True  
             self.scale = False
-        
-        if self.mode == "Move":
-            for area in areas:
-                for space in area.spaces:
-                    if space.type == 'VIEW_3D':
-                        space.show_gizmo_object_translate^= True
-                        space.show_gizmo_object_rotate = False
-                        space.show_gizmo_object_scale = False
-        if self.mode == "Rotate":
-            for area in areas:
-                for space in area.spaces:
-                    if space.type == 'VIEW_3D':
-                        space.show_gizmo_object_translate = False
-                        space.show_gizmo_object_rotate^= True
-                        space.show_gizmo_object_scale = False
-        if self.mode == "Scale":
-            for area in areas:
-                for space in area.spaces:
-                    if space.type == 'VIEW_3D':
-                        space.show_gizmo_object_translate = False
-                        space.show_gizmo_object_rotate = False
-                        space.show_gizmo_object_scale^= True
-        if self.mode == "AddMove":
-            for area in areas:
-                for space in area.spaces:
-                    if space.type == 'VIEW_3D':
-                        space.show_gizmo_object_translate^= True
-        if self.mode == "AddRotate":
-            for area in areas:
-                for space in area.spaces:
-                    if space.type == 'VIEW_3D':
-                        space.show_gizmo_object_rotate^= True
-        if self.mode == "AddScale":
-            for area in areas:
-                for space in area.spaces:
-                    if space.type == 'VIEW_3D':
-                        space.show_gizmo_object_scale^= True
         return {'FINISHED'}
 
 class Cycle_OT_Operator(bpy.types.Operator):
